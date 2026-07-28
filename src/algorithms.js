f0cc1ab54d5ac4180f56f5ea7901cfbdedca363e
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-07-28 23:57:56 +0200

    add findLongestWord solution

--- src/algorithms.js
+++ src/algorithms.js
@@ -25,8 +25,25 @@ console.log(isAnagram("finder", "friend"));
 console.log(isAnagram("hello", "buy"));
 */
 
+
+/*
 export function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 5, 5]));
+*/
+
+export function findLongestWord(str) {
+    const word = str.split(' ');
+    let longest = '';
+
+    for (const word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+    return longest;
+}
+
+console.log(findLongestWord("The quick brown fox jumps over the lazy dog"));
