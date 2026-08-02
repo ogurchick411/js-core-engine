991e9bc60ee831cc514ec3c8df746af6604a4981
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-08-02 20:21:22 +0200

    fix array reversal algorithm

--- src/algorithms.js
+++ src/algorithms.js
@@ -230,19 +230,21 @@ console.log("common elements:", findCommonElements(listA, listB));
 
 
 function reverseArrayInPlace(arr) {
-  let left = 0;
-  let right = arr.length - 1;
+    let left = 0;
+    let right = arr.length - 1;
 
-  whie (left < right) {
-    const temp = arr[left];
+    while (left < right) {
+        const temp = arr[left];
+        
+        arr[left] = arr[right];
+        arr[right] = temp;
 
-    arr[left] = arr[right];
-    arr[right] = temp;
+        left++;
+        right--;
+    }
 
-    left++;
-    right--;
-  }
-
-  const originalArray = [1, 2, 3, 4, 5];
-  console.log("reversed array:", reverseArrayInPlace(originalArray));
+    return arr;
 }
+
+const originalArray = [1, 2, 3, 4, 5];
+console.log("Reversed array:", reverseArrayInPlace(originalArray));
