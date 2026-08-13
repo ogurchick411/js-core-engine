30fa7ece2d79bec81132f0c68eff12f83d89f4c2
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-08-13 23:44:01 +0200

    .find

--- src/algorithms.js
+++ src/algorithms.js
@@ -845,4 +845,14 @@ console.log(sum);
 */
 
 
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" }
+];
+
+const foundUser = users.find(user => user.id === 2);
+console.log(foundUser);
+
+
 
